28de42eb7bee71a3db848dd97b9ee7260d566d57
Sam Wight <samuelwight@gmail.com>
2018-04-02 15:37:35 -0500

    Created iNow Credential table, model, views, and integrated it with rest of relevant views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+# Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :courses, dependent: :destroy
+
+  has_one :credential, dependent: :destroy
 end
